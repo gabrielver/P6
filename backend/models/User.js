c787c6830d20d8bdddfed9036fae9e4db1b3b402
Gabriel Verronet <verronet.g@gmail.com>
2022-01-07 08:52:46 +0100

    change

--- backend/models/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema({
-    email: { type : String},
-    password: { type : String},
-});
-
-module.exports = mongoose.model('User', userSchema);
